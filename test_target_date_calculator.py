32149f50594dfa3ff94f243025e0ef0594fc8552
str <str_ie@yahoo.co.uk>
2021-01-08 19:26:00 +0100

    Add test descriptions

--- test_target_date_calculator.py
+++ test_target_date_calculator.py
@@ -10,24 +10,29 @@ import todays
 class TestTargetDateCalculator(unittest.TestCase):
     @parameterized.expand([
         # Dec to Jan
-        (2020, 12, date(2020, 12, 31), None, date(2021, 1, 31)),
-        (2020, 12, date(2020, 12, 1), None, date(2021, 1, 1)),
-        (2021, 1, date(2020, 12, 1), None, date(2021, 1, 1)),
+        ('Dec to Jan', 2020, 12, date(2020, 12, 31), None, date(2021, 1, 31)),
+        ('Dec to Jan', 2020, 12, date(2020, 12, 1), None, date(2021, 1, 1)),
+        ('Dec to Jan', 2021, 1, date(2020, 12, 1), None, date(2021, 1, 1)),
         # End Jan to Feb - overflow
-        (2021, 1, date(2021, 1, 30), None, date(2021, 2, 28)),
-        (2021, 2, date(2021, 1, 30), None, date(2021, 2, 28)),
+        ('End Jan to Feb - overflow', 2021, 1,
+         date(2021, 1, 30), None, date(2021, 2, 28)),
+        ('End Jan to Feb - overflow', 2021, 2,
+         date(2021, 1, 30), None, date(2021, 2, 28)),
         # End Jan to Feb - no overflow
-        (2021, 1, date(2021, 1, 28), None, date(2021, 2, 28)),
-        (2021, 2, date(2021, 1, 28), None, date(2021, 2, 28)),
-        (2021, 1, date(2021, 1, 27), None, date(2021, 2, 27)),
-        (2021, 2, date(2021, 1, 27), None, date(2021, 2, 27)),
-        # xxx
+        ('End Jan to Feb - no overflow', 2021, 1,
+         date(2021, 1, 28), None, date(2021, 2, 28)),
+        ('End Jan to Feb - no overflow', 2021, 2,
+         date(2021, 1, 28), None, date(2021, 2, 28)),
+        ('End Jan to Feb - no overflow', 2021, 1,
+         date(2021, 1, 27), None, date(2021, 2, 27)),
+        ('End Jan to Feb - no overflow', 2021, 2,
+         date(2021, 1, 27), None, date(2021, 2, 27)),
     ])
-    def test_source_year(self, this_year, this_month, source_date, target_date_option, expected_target_date):
+    def test_source_year(self, msg, this_year, this_month, source_date, target_date_option, expected_target_date):
         # Act
         today = todays.TodayMock(this_year, this_month)
         date_context = date_utils.DateContext(today, source_date.month)
 
         actual_target_date = target_date_calculator.calculate_target_date(
             date_context, source_date, target_date_option)
-        self.assertEqual(expected_target_date, actual_target_date)
+        self.assertEqual(expected_target_date, actual_target_date, msg)
