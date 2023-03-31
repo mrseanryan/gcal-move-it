d0271d71633dded6015c50e74610f041bd354940
str <str_ie@yahoo.co.uk>
2023-03-31 20:48:14 +0200

    Add ill to the blacklist

--- go_move_with_blacklist_dryrun.bat
+++ go_move_with_blacklist_dryrun.bat
@@ -1,1 +1,1 @@
-python gcal_move_it.py move -b "^k ;^cancelled ;^done ;^n/a;=hol" -d %1 %2 %3 %4 %5
+python gcal_move_it.py move -b "^k ;^cancelled ;^done ;^n/a;=hol;^ill" -d %1 %2 %3 %4 %5
--- go_move_with_blacklist_live.sh
+++ go_move_with_blacklist_live.sh
@@ -2,4 +2,4 @@ ECHO "This is NOT a dry run!"
 ECHO ""
 read -p "Press enter to continue OR CTRL+C to cancel"
 
-python3 gcal_move_it.py move -b "^k ;^cancelled ;^done ;^n/a;=hol" $1 $2 $3 $4 $5
+python3 gcal_move_it.py move -b "^k ;^cancelled ;^done ;^n/a;=hol;^ill" $1 $2 $3 $4 $5
